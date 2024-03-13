a4eb1eec3ae6155f1f34c938530f23d1d573ebb6
jdsatashi <vdt1073@gmail.com>
2024-03-13 11:08:36 +0700

    Add new app KhachHang Profile

---
+++ src/user_system/kh_profile/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from account.models import User
+from user_system.kh_nhomkh.models import NhomKH
+
+
+# Create your models here.
+class KHProfile(models.Model):
+    maKH = models.ForeignKey(User, to_field='usercode', on_delete=models.CASCADE)
+    tenBanKe = models.CharField(max_length=255, null=False)
+    tenDoanhNghiep = models.CharField(max_length=255, null=True)
+    maNhomKH = models.ForeignKey(NhomKH, on_delete=models.CASCADE)
+    maNVTT = models.CharField(max_length=255, null=True)
+    diaChi = models.TextField(null=True)
+    nguoiLap = models.TextField(null=True)
+    maKHCap1 = models.CharField(max_length=255, null=True)
