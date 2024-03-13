1e53c61924fedf853316a8bd30ec40f1b0c4d06f
jdsatashi <vdt1073@gmail.com>
2024-03-13 11:07:54 +0700

    Add new app User Type

---
+++ src/user_system/user_type/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class UserType(models.Model):
+    loaiUser = models.CharField(max_length=100, unique=True, null=False, blank=False)
+    moTa = models.TextField(null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
