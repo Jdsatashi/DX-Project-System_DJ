5454b2f9ac253fdbbc767c132ed21c634e6aa9b7
jdsatashi <vdt1073@gmail.com>
2024-03-19 10:08:40 +0700

    Add handle and view return

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from account.handle import handle_create_acc
+
+
 # Create your views here.
+def create_acc(request):
+    ctx = handle_create_acc(request)
+    return render(request, 'account/create.html', ctx)
+
+
+def list_acc(request):
+    ctx = list_acc(request)
+    return render(request, 'account/index.html', ctx)
