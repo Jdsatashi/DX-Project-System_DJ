419b5de560c1c791abdde554d2c382f3f4cab99c
jdsatashi <vdt1073@gmail.com>
2024-03-19 10:09:02 +0700

    Add url for account view

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from .api import insertDB
+from .views import create_acc, list_acc
 
 app_name = 'account'
 
 urlpatterns = [
-    path('', insertDB, name='insertDB')
+    path('', insertDB, name='insertDB'),
+    path('create', create_acc, name='create_acc'),
+    path('list', list_acc, name='list_acc')
 ]
