51b7c7a4c44918968471367eb2ffab0b7b8449ef
jdsatashi <vdt1073@gmail.com>
2024-03-21 11:26:45 +0700

    Update and modify DRF, CORS, JWT in settings project

--- src/__init__/settings.py
+++ src/__init__/settings.py
@@ -7,7 +7,6 @@ import dotenv
 
 dotenv.load_dotenv()
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,65 +26,79 @@ AUTH_USER_MODEL = 'account.User'
 
 # Application definition
 INSTALLED_APPS = [
-	'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.messages',
-	'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 
-	# My app
-	'draft',
-	'account',
-	# Old data app
-	'qlsx.khach_hang',
-	# User system structure
-	'user_system.kh_nhomkh',
-	'user_system.kh_profile',
-	'user_system.nv_profile',
-	'user_system.user_type',
+    # Django Rest Framework
+    'rest_framework',
+    # Cors header
+    'corsheaders',
+    # My app
+    'draft',
+    'account',
+    # Old data app
+    'qlsx.khach_hang',
+    'old_data',
+    # User system structure
+    'user_system.kh_nhomkh',
+    'user_system.kh_profile',
+    'user_system.nv_profile',
+    'user_system.user_type',
 ]
 
 LOGGING = {
-	"version": 1,
-	"disable_existing_loggers": False,
-	"handlers": {
-		"console": {
-			"class": "logging.StreamHandler",
-		},
-	},
-	"root": {
-		"handlers": ["console"],
-		"level": "INFO",
-	},
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
 }
 
 MIDDLEWARE = [
-	'django.middleware.security.SecurityMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.common.CommonMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = '__init__.urls'
 
 TEMPLATES = [
-	{
-		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': [BASE_DIR / 'templates'],
-		'APP_DIRS': True,
-		'OPTIONS': {
-			'context_processors': [
-				'django.template.context_processors.debug',
-				'django.template.context_processors.request',
-				'django.contrib.auth.context_processors.auth',
-				'django.contrib.messages.context_processors.messages',
-			],
-		},
-	},
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = '__init__.wsgi.application'
@@ -94,57 +107,57 @@ WSGI_APPLICATION = '__init__.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 MSSQL = {
-		'ENGINE': 'mssql',
-		'NAME': os.environ['DB_NAME'],
-		'HOST': os.environ['DB_HOST'],
-		'PORT': os.environ['DB_PORT'],
-		'USER': os.environ['DB_USER'],
-		'PASSWORD': os.environ['DB_PASSWORD'],
-		'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server'}
+    'ENGINE': 'mssql',
+    'NAME': os.environ['DB_NAME'],
+    'HOST': os.environ['DB_HOST'],
+    'PORT': os.environ['DB_PORT'],
+    'USER': os.environ['DB_USER'],
+    'PASSWORD': os.environ['DB_PASSWORD'],
+    'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server'}
 }
 
 SQLITE = {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
 }
 
 POSTGRESQL = {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': os.environ['PGS_DB'],
-		'HOST': os.environ['PGS_HOST'],
-		'PORT': os.environ['PGS_PORT'],
-		'USER': os.environ['PGS_USER'],
-		'PASSWORD': os.environ['PGS_PASSWORD'],
-		'OPTIONS': {
-			# 'sslmode': os.environ['PGS_SSL'],
-			# 'options': f'?options=endpoint%3D{os.environ["PGS_ENDPOINT_ID"]}',
-		},
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ['PGS_DB'],
+    'HOST': os.environ['PGS_HOST'],
+    'PORT': os.environ['PGS_PORT'],
+    'USER': os.environ['PGS_USER'],
+    'PASSWORD': os.environ['PGS_PASSWORD'],
+    'OPTIONS': {
+        # 'sslmode': os.environ['PGS_SSL'],
+        # 'options': f'?options=endpoint%3D{os.environ["PGS_ENDPOINT_ID"]}',
+    },
 }
 
 DATABASES = {
-	'default': POSTGRESQL
+    'default': POSTGRESQL
 }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-	{
-		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-	},
-	{
-		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-	},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 PASSWORD_HASHERS = [
-	"django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
 ]
 
 # Internationalization
