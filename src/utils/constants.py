7d98f3af52863ebbcb9310cbf5d022396dbe353c
jdsatashi <vdt1073@gmail.com>
2024-03-24 13:03:42 +0700

    Remove object model from constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,3 +1,6 @@
+from user_system.kh_nhomkh.models import NhomKH
+from user_system.user_type.models import UserType
+
 old_data = {
     "tb_kh": "tbl_QLTT_KhachHang",
     "tb_nhanvien": "Sys_NhanVien",
@@ -5,3 +8,7 @@ old_data = {
     "tb_nhomKH": "tbl_QLTT_DanhMucNhomKhachHang"
 }
 api_path = 'api/v1/'
+type_nv, _ = UserType.objects.get_or_create(loaiUser="nhanvien")
+type_kh, _ = UserType.objects.get_or_create(loaiUser="khachhang")
+maNhomND = 'DMNKH000005'
+tenNhomND = 'Khách hàng Nông dân'
