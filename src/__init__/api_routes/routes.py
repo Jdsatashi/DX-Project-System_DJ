abfa9aa61e777b7a130677d7174a0aba736cfd0b
jdsatashi <vdt1073@gmail.com>
2024-03-26 10:58:43 +0700

    Add new api urls

--- src/__init__/api_routes/routes.py
+++ src/__init__/api_routes/routes.py
@@ -4,5 +4,6 @@ app_name = 'api_routes'
 
 urlpatterns = [
     path('accounts/', include('account.api.api_routes', 'api_account')),
-    path('token/', include('__init__.api_routes.urls', 'api_token'))
+    path('token/', include('__init__.api_routes.urls', 'api_token')),
+    path('draft/', include('draft.api.routes'), name='api_draft')
 ]
