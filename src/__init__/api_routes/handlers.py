70819b164593fd51b03e2b64034163527eee0681
jdsatashi <vdt1073@gmail.com>
2024-03-26 10:59:17 +0700

    Edit user_id of token

--- src/__init__/api_routes/handlers.py
+++ src/__init__/api_routes/handlers.py
@@ -4,35 +4,63 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from account.api.serializers import UserSerializer
 from account.models import User
 
 
-class CustomTokenObtainPairSerializer(serializers.Serializer):
-    usercode = serializers.CharField(max_length=64)
-    password = serializers.CharField(max_length=255)
-
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        usercode = attrs['usercode'].upper()
+        usercode = attrs['username'].upper()
         password = attrs['password']
 
         # Get user object for validating
         user = User.objects.filter(usercode=usercode).first()
-        user.id = user.usercode
+
         # Validating login request
         if user is None:
             raise AuthenticationFailed(f'User not found!')
         if not check_password(password, user.password):
-            return Response({'message': 'Wrong password!'})
-        user_data = UserSerializer(user).data
-        tokens = get_tokens_for_user(user)
-        tokens['users'] = user_data
-        return tokens
+            raise AuthenticationFailed('Wrong password!')
+
+        # Generate token
+        token = super().get_token(user)
+        # Add custom data to token payload
+        token['user_data'] = {
+            'usercode': user.usercode,
+            'email': user.email,
+            'phone_number': user.phone_number,
+            'khuVuc': user.khuVuc,
+            'status': user.status,
+            'loaiUser': user.loaiUser.loaiUser if user.loaiUser else None,
+        }
+        token['user_id'] = user.usercode
+
+        return {
+            'refresh': str(token),
+            'access': str(token.access_token),
+        }
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['user_data'] = {
+            'usercode': user.usercode,
+            'username': user.username,
+            'email': user.email,
+            'phone_number': user.phone_number,
+            'khuVuc': user.khuVuc,
+            'status': user.status,
+            'loaiUser': user.loaiUser,
+        }
+        return token
 
 
 def get_tokens_for_user(user):
