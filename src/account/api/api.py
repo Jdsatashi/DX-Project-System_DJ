4ac403cba18890528f7af0417c2eafca8de8d62a
jdsatashi <vdt1073@gmail.com>
2024-03-31 20:28:13 +0700

    Delete unused model

--- src/account/api/api.py
+++ src/account/api/api.py
@@ -11,7 +11,7 @@ from rest_framework.views import APIView
 
 from account.api.serializers import UserSerializer
 from account.handle import handle_create_acc, handle_list_acc
-from account.models import User, LoginToken
+from account.models import User
 
 
 def api_create_user(req):
@@ -44,64 +44,6 @@ class ApiRegister(APIView):
         serializer.save()
         return Response(serializer.data)
 
-
-# Login api view
-class ApiLogin(APIView):
-    def post(self, request):
-        # Get request data
-        user_id = request.data.get('user_id')
-        user_id = user_id.upper()
-        password = request.data.get('password')
-        # Get user object for validating
-        user = User.objects.filter(id=user_id).first()
-        # Validating login request
-        if user is None:
-            return Response({'message': 'User not found'}, status.HTTP_401_UNAUTHORIZED)
-        if not check_password(password, user.password):
-            return Response({'message': 'Wrong password!'}, status=status.HTTP_401_UNAUTHORIZED)
-        # Set data for create jwt token
-        expired = datetime.datetime.utcnow() + datetime.timedelta(minutes=60)
-        payload = {
-            'id': user_id,
-            'exp': expired,
-            'iat': datetime.datetime.utcnow()
-        }
-        # Create token
-        token = jwt.encode(payload, os.environ.get('SECRET_KEY'), algorithm='HS256').decode('utf-8')
-        # Save token to datababse
-        login_data = LoginToken.objects.create(user=user, token=token, status='active', expired_at=expired)
-        # Create response
-        response = Response()
-        response.set_cookie(key='jwt', value=token, httponly=True)
-        response.data = {
-            'message': f'Login success! Welcome {user.id}',
-            'data': {
-                'user': UserSerializer(user).data,
-                'login_data': {
-                    'token': login_data.token,
-                    'expired_at': login_data.expired_at,
-                }
-            }
-        }
-        response.status_code = status.HTTP_200_OK
-        return response
-
-
-# Logout api
-class ApiLogout(APIView):
-    def post(self, request):
-        user_id = request.data.get('user_id')
-        token = request.COOKIES.get('jwt')
-        user = User.objects.filter(id=user_id.upper()).first()
-        if not user:
-            return Response({'message': 'User not found'}, status=status.HTTP_401_UNAUTHORIZED)
-        LoginToken.objects.get(user=user, token=token).delete()
-        response = Response()
-        response.delete_cookie('jwt')
-        response.data = {'message': 'Logged out successfully'}
-        return response
-
-
 # Checking token function
 def verify_token(token):
     try:
@@ -115,22 +57,6 @@ def verify_token(token):
         'valid': True,
         'data': payload
     }
-
-
-# Test token
-class ApiTest(APIView):
-    def get(self, request):
-        token = request.COOKIES.get('jwt')
-        if not token:
-            return Response({'message': 'unauthorized'})
-        verify = verify_token(token)
-        user_id = verify.get('data').get('id')
-        if not verify:
-            return Response({'message': verify.get('message')})
-        user = User.objects.get(id=user_id)
-        serializer = UserSerializer(user)
-        return Response({'data': serializer.data})
-
 
 # Get public ip of client
 def get_client_ip(request):
--- src/account/api/api_routes.py
+++ src/account/api/api_routes.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from account.api.api import api_list_user, api_create_user, ApiRegister, ApiLogin, ApiTest, ApiLogout
+from account.api.api import api_list_user, api_create_user, ApiRegister
 
 app_name = 'api_account'
 
@@ -8,7 +8,4 @@ urlpatterns = [
     path('list', api_list_user, name='api_list_acc'),
     path('create', api_create_user, name='api_create_acc'),
     path('register', ApiRegister.as_view(), name='api_register'),
-    path('login', ApiLogin.as_view(), name='api_login'),
-    path('logout', ApiLogout.as_view(), name='api_logout'),
-    path('test', ApiTest.as_view(), name='api_test')
 ]
