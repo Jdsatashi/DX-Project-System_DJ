d82ce8c5bb1a2c0efd525b1418699cefec364f2b
jdsatashi <vdt1073@gmail.com>
2024-04-01 22:52:36 +0700

    Create migration auto add default quyen

---
+++ src/account/migrations/migrate_perm.py
@@ -0,0 +1,27 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db import migrations
+
+from account.models import Quyen
+
+
+def create_initial_permission(apps, schema_editor):
+    content_types = ContentType.objects.all()
+
+    for i, content_type in enumerate(content_types):
+        if i > 6:
+            quyen_name = f'{content_type.app_label}_{content_type.model}'
+            tasks = ['list', 'create', 'retrieve', 'update', 'delete']
+            for task in tasks:
+                quyen_name_ = f'{task}_{quyen_name}'
+                Quyen.objects.create(name=quyen_name_, mota=f'{task.capitalize()} {content_type.model}')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_initial_permission),
+    ]
