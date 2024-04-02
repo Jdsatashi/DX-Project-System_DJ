9898ce1153acff05cc3df8b8767f392e6d27c020
jdsatashi <vdt1073@gmail.com>
2024-04-02 20:35:09 +0700

    Update action quyen constants.py

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,6 +1,3 @@
-from user_system.kh_nhomkh.models import NhomKH
-from user_system.user_type.models import UserType
-
 old_data = {
     "tb_kh": "tbl_QLTT_KhachHang",
     "tb_nhanvien": "Sys_NhanVien",
@@ -10,3 +7,15 @@ old_data = {
 api_path = 'api/v1/'
 maNhomND = 'DMNKH000005'
 tenNhomND = 'Khách hàng Nông dân'
+# Action of Quyen
+acquy = {
+    'full': ['list', 'create', 'retrieve', 'update', 'partial_update', 'destroy'],
+    'self': ['list', 'create', 'retrieve', 'update', 'partial_update'],
+    'view': ['list', 'retrieve'],
+    'create': 'create',
+    'retrieve': 'retrieve',
+    'list': 'list',
+    'update': 'update',
+    'partial_update': 'partial_update',
+    'destroy': 'destroy'
+}
