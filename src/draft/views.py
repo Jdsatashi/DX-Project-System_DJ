6289206c99ed5aa3967a974590be04c1ce840a36
jdsatashi <vdt1073@gmail.com>
2024-04-02 20:34:32 +0700

    Update update api view

--- src/draft/views.py
+++ src/draft/views.py
@@ -1,24 +1,15 @@
 from django.shortcuts import render, redirect
 
 from account.handlers.validate_quyen import quyen
-# Create your views here.
 from draft.handlers.handle import handle_create_group_draft, handle_get_list_draft, handle_create_draft, handle_draft_id, \
     handle_delete_draft
 from draft.models import GroupDraft
-
-# draft_content = ContentType.objects.get_for_model(Draft)
-# draft_group_content = ContentType.objects.get_for_model(GroupDraft)
-
-# draft_name = f"{draft_content.app_label}_{draft_content.model}"
-# draft_group_name = f"{draft_group_content.app_label}_{draft_group_content.model}"
-draft_group_name = ""
 
 
 @quyen(GroupDraft, 'create')
 def create_group_draft(request):
     context = handle_create_group_draft(request)
     return render(request, 'draft/group/create.html', context)
-    # return HttpResponse(context)
 
 
 def list_draft(request):
