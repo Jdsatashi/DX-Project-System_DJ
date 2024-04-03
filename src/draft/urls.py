dcc6fd0ba0ba434d05896731f6ee5772fb808e0d
jdsatashi <vdt1073@gmail.com>
2024-04-03 22:37:12 +0700

    Update path

--- src/draft/urls.py
+++ src/draft/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', list_draft, name='list_draft'),
     path('group/create', create_group_draft, name='create_group_draft'),
     path('create', create_draft, name='create_draft'),
-    path('detail/<int:id>', draft_item, name='draft_item'),
-    path('detail/<int:id>/delete', draft_delete, name='draft_delete'),
+    path('detail/<pk>', draft_item, name='draft_item'),
+    path('detail/<pk>/delete', draft_delete, name='draft_delete'),
 ]
