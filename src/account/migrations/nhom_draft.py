b1f6a2bbee2beac96282ddf9b324cbc0dde5fb9e
jdsatashi <vdt1073@gmail.com>
2024-04-03 22:11:55 +0700

    Create new migrate nhom of draft for testing

---
+++ src/account/migrations/nhom_draft.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+
+from account.models import Quyen, NhomQuyen
+
+
+def create_initial_permission(apps, schema_editor):
+    matching_quyens = Quyen.objects.filter(name__icontains="draft").all()
+    list_drafts_quyens = list(matching_quyens)
+    nhom_quyen, _ = NhomQuyen.objects.get_or_create(name="nhom_draft")
+    for q in list_drafts_quyens:
+        nhom_quyen.quyen.add(q.name)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0001_alter_user_last_login'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_initial_permission),
+    ]
