1793333ed9f96d6264ae9d39b0e7fc878c4275de
jdsatashi <vdt1073@gmail.com>
2024-04-04 10:31:53 +0700

    Delete khachhang app in qlsx

--- src/qlsx/khach_hang/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KhachHangConfig(AppConfig):
-    name = 'qlsx.khach_hang'
--- src/qlsx/khach_hang/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from qlsx.khach_hang.models import KhachHangModel
-
-
-class CreateKhachHang(forms.ModelForm):
-    class Meta:
-        model = KhachHangModel
-        fields = '__all__'
