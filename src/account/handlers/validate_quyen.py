d70fc340a88c2536da2ead71ec3274fc7ac980fc
jdsatashi <vdt1073@gmail.com>
2024-04-04 11:37:30 +0700

    Update add comment to function

--- src/account/handlers/validate_quyen.py
+++ src/account/handlers/validate_quyen.py
@@ -11,18 +11,26 @@ def quyen(model, method):
     """ quyen use as class method @quyen for view function"""
     model_content = ContentType.objects.get_for_model(model)
 
+    # Create decorator function
     def decorator(view_func):
         @wraps(view_func)
         def wrapped_view(request, *args, **kwargs):
+            # Get string quyen name
             required_perm = f"{method}_{model_content.app_label}_{model_content.model}"
+            # Get primary key if exist
             pk = kwargs.get('pk')
+            # Check authenticate
             if not request.user.is_authenticated:
                 ctx = {'message': "Bạn chưa đăng nhập."}
                 return render(request, 'errors/403.html', ctx)
+            # Block message
             ctx = {'message': "Bạn không có quyền truy cập vào trang này."}
+            # Check user has quyen
             check_quyen = request.user.has_quyen(required_perm)
+            # Return when false
             if not check_quyen:
                 return render(request, 'errors/403.html', ctx)
+            # If user has quyen, check PK
             if pk:
                 required_perm = f"{required_perm}_{pk}"
                 if not check_quyen and not request.user.has_quyen(required_perm):
@@ -50,41 +58,46 @@ class ValidateQuyenRest(permissions.BasePermission):
         if not user.is_authenticated:
             return False
         # Get module_name
-        try:
-            view_func = getattr(view, view.action)
-        except AttributeError:
-            view_func = None
-        try:
-            module_name = view_func.__qualname__.split('.')[2]
-        except IndexError:
-            module_name = view.__module__.split('.')[0]
-        print("---------- TEST ----------")
+        modules = view.__module__.split('.')
+        contents_type = ContentType.objects.values_list('app_label', flat=True).distinct()
+        module_name = ''
+        for module in modules:
+            if module in contents_type:
+                module_name = module
+                break
+
         object_pk = view.kwargs.get('pk', None)
-        print(f"object_pk: {object_pk}")
         # Get action of function
         action = view.action
         content_type = ContentType.objects.get_for_model(self.model)
-
+        # Merge string from above data to generated permission name
         required_permission = f'{action}_{module_name}_{content_type.model}'
-        print(f"Required perm 1: {required_permission}")
-        model_permission = user.has_quyen(required_permission)
+        # Check if user or user_nhom has quyen
+        user_nhom_perm = user.has_nhom_with_quyen(required_permission)
+        user_perm = user.has_quyen(required_permission)
+        # Check if object has PK
         if object_pk is not None:
-            print(f"Object pk is not None")
+            # Add PK to string quyen
             required_permission = f"{required_permission}_{object_pk}"
-            print(f"Required perm 2: {required_permission}")
+            # Checking quyen with PK is exist
             quyen = quyen_exist({'name': required_permission})
-            print(f"Quyen: {quyen}")
+            # If quyen PK exist, handling validate quyen user
             if quyen is not None:
-                is_allow = user.is_allow(required_permission)
-                print(f"is_allow: {is_allow}")
-                return is_allow
+                # Check if user or user_nhom has quyen PK
+                user_perm = user.is_allow(required_permission)
+                user_nhom_perm = user.has_nhom_with_quyen(required_permission)
+                # If user or nhom user has quyen, return True
+                return user_nhom_perm or user_perm
+            # Return True (not required quyen) when object with PK doesn't required Quyen PK
             return True
-        print(f"Return model permission")
-        return model_permission
+        # If user or nhom user has quyen, return True
+        return user_nhom_perm or user_perm
 
 
 def quyen_exist(quyen: dict):
+    # Validate quyen is dictionary and quyen has value
     if not isinstance(quyen, dict) or quyen is None:
         raise ValueError("quyen must be a dictionary {'key': value}.")
+
     q = Quyen.objects.filter(name=quyen.get('name'))
     return q.first() if q.exists() else None
