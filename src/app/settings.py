dd137eb44f9529d522ba43ae488d3b8e881bde27
jdsatashi <vdt1073@gmail.com>
2024-04-07 15:04:01 +0700

    Update host in setting

--- src/app/settings.py
+++ src/app/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '192.168.1.17', 'jdserver.ddns.com']
 
 # Customize authentication
 AUTH_USER_MODEL = 'account.User'
