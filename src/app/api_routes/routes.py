284a2421e5a7a7c3eb53985b66b489cf6e12697d
jdsatashi <vdt1073@gmail.com>
2024-04-07 21:10:38 +0700

    Add api schema

--- src/app/api_routes/routes.py
+++ src/app/api_routes/routes.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 app_name = 'api_routes'
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='layouts/api_docs.html', extra_context={'schema_url': 'api_schema'}), name='api_docs'),
     path('accounts/', include('account.api.routes', 'api_account')),
     path('token/', include('app.api_routes.urls', 'api_token')),
     path('draft/', include('draft.api.routes'), name='api_draft'),
--- src/app/urls.py
+++ src/app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.shortcuts import render
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 
 # Create homepage view
@@ -13,5 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('draft/', include('draft.urls', 'draft')),
     path('accounts/', include('account.urls', 'account')),
+    path('api_schema', get_schema_view(title='API schema', description='Guide for REST Api'), name='api_schema'),
     path('application/api/v1/2024/', include('app.api_routes.routes', 'api_routes')),
 ]
