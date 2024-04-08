54d65afe8e8e51ca0427ab3d92ea885e41747b06
jdsatashi <vdt1073@gmail.com>
2024-04-08 20:30:56 +0700

    Update full api of user type

--- src/user_system/user_type/api/routes.py
+++ src/user_system/user_type/api/routes.py
@@ -1,9 +1,19 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from user_system.user_type.api.views import UserTypeApiView
+from user_system.user_type.api.views import UserTypeApiView, GenericApiUserType
 
 app_name = 'user_type'
 
+router = DefaultRouter()
+router.register('genericview', GenericApiUserType, basename='api_user_type')
+
+# Draft view
+types_view = GenericApiUserType.as_view({'get': 'list', 'post': 'create'})
+type_detail_view = GenericApiUserType.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})
+
+
 urlpatterns = [
-    path('', UserTypeApiView.as_view(), name='get_user_type')
+    path('', types_view),
+    path('<pk>/', type_detail_view),
 ]
--- src/user_system/user_type/api/views.py
+++ src/user_system/user_type/api/views.py
@@ -1,9 +1,15 @@
+from functools import partial
+
 from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator
 from django.db.models import Q
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from account.handlers.validate_perm import ValidatePermRest
 from user_system.user_type.api.serializers import UserTypeSerializer
 from user_system.user_type.models import UserType
 
@@ -15,6 +21,14 @@ class UserTypeApiView(APIView):
 
         serializer = UserTypeSerializer(user_types, many=True)
         return Response(serializer.data)
+
+
+class GenericApiUserType(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = UserTypeSerializer
+    queryset = UserType.objects.all()
+    # authentication_classes = [JWTAuthentication, BasicAuthentication, SessionAuthentication]
+    # permission_classes = [partial(ValidatePermRest, model=UserType)]
 
 
 def filter_user_type(r):
