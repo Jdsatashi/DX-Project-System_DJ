61795372f64018d2556a7e9a817bc17ba27f7deb
jdsatashi <vdt1073@gmail.com>
2024-04-10 16:42:56 +0700

    Fix error on api schema view

--- src/app/api_routes/routes.py
+++ src/app/api_routes/routes.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('user-type/', include('user_system.user_type.api.routes', 'api_user_type')),
     path('client-group/', include('user_system.client_group.api.routes', 'api_client_group')),
     path('employees/', include('user_system.employee_profile.api.routes', 'api_employee_profile')),
-
+    # path('file-upload/', include('system.file_upload.api.routes', 'api_file_upload')),
 ]
