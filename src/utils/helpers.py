7d6504026dea8293a837f052f4b867b049fefd8a
jdsatashi <vdt1073@gmail.com>
2024-04-11 19:14:54 +0700

    Testing fix error cannott connectt MSSQL

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -14,13 +14,14 @@ def table_data(table_name: str):
     db_name = os.environ.get('MSSQL_DATABASE')
     user = os.environ.get('MSSQL_USER')
     password = os.environ.get('MSSQL_PASSWORD')
-    drivers = ["SQL Server", "ODBC Driver 18 for SQL Server", "ODBC Driver 17 for SQL Server"]
+    drivers = ["SQL Server", "ODBC Driver 17 for SQL Server"]
     for i, driver in enumerate(drivers):
         try:
             connection_string = f"DRIVER={{{driver}}};SERVER={server};DATABASE={db_name};UID={user};PWD={password}"
             if i >= 1:
                 connection_string += ";Encrypt=no"
             print(f"Attempting to connect using driver: {driver}")
+            print(f"Connection string: {connection_string}")
             con = pyodbc.connect(connection_string)
             cursor = con.cursor()
             query = f"SELECT * FROM {table_name}"
