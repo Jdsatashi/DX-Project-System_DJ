6fc93252cf359523026c6ae6f0554a119f275e7d
jdsatashi <vdt1073@gmail.com>
2024-04-14 13:05:16 +0700

    Create add models data to admin panel

--- src/marketing/product/admin.py
+++ src/marketing/product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from marketing.product.models import Product, ProductType, RegistrationUnit, Producer, RegistrationCert, ProductCategory
+
 # Register your models here.
+admin.site.register(ProductType)
+admin.site.register(RegistrationUnit)
+admin.site.register(Producer)
+admin.site.register(RegistrationCert)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
