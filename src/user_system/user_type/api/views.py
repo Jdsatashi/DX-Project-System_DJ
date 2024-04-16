7e1fd5ea108e39cc442cd3ea66183a623b681674
jdsatashi <vdt1073@gmail.com>
2024-04-16 21:39:31 +0700

    Update optimize code remove un use function

--- src/user_system/user_type/api/views.py
+++ src/user_system/user_type/api/views.py
@@ -17,7 +17,7 @@ from user_system.user_type.models import UserType
 class UserTypeApiView(APIView):
     def get(self, request):
 
-        user_types = filter_user_type(request)
+        user_types = UserType.objects.all()
 
         serializer = UserTypeSerializer(user_types, many=True)
         return Response(serializer.data)
@@ -29,36 +29,3 @@ class GenericApiUserType(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.
     queryset = UserType.objects.all()
     authentication_classes = [JWTAuthentication, BasicAuthentication, SessionAuthentication]
     permission_classes = [partial(ValidatePermRest, model=UserType)]
-
-
-def filter_user_type(r):
-    print(f"---------- Here Testing ----------\n")
-    query_params = r.query_params
-    search_query = query_params.get('query', {})
-    limit = query_params.get('limit', 10)
-    page_number = query_params.get('page', 1)
-    print([search_query, limit, page_number, search_query])
-    return filter_data(search_query, limit, page_number, UserType)
-
-
-def filter_data(query: dict, limit: int, page_number: int, _model):
-    print(f"---------- Here Testing ----------\n")
-    content_type = ContentType.objects.get_for_model(_model)
-    print(content_type)
-    model_class = content_type.model_class()
-    query_set = model_class.objects.all()
-    print(query.items())
-    if query.items():
-        search_conditions = []
-
-        for key, value in query.items():
-            search_condition = Q(**{f"{key}__icontains": value})
-            search_conditions.append(search_condition)
-        final_search_query = search_conditions.pop()
-        for condition in search_conditions:
-            final_search_query |= condition
-        query_set = query_set.filter(final_search_query)
-
-    paginate = Paginator(query_set, limit)
-    page_obj = paginate.get_page(page_number)
-    return page_obj.object_list
