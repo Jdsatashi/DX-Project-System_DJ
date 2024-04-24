618e7d63ee652737969b7e7a7df506a35f625675
jdsatashi <vdt1073@gmail.com>
2024-04-24 21:58:18 +0700

    Update add new management price list for admin site

--- src/marketing/price_list/admin.py
+++ src/marketing/price_list/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from marketing.price_list.models import PriceList, ProductPrice
+
 # Register your models here.
+admin.site.register(PriceList)
+admin.site.register(ProductPrice)
