55ad6607fc2dd91e5ea02e313fec6893ffc9e1eb
jdsatashi <vdt1073@gmail.com>
2024-04-25 14:42:18 +0700

    Update add category detail to admin site

--- src/marketing/product/admin.py
+++ src/marketing/product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from marketing.product.models import Product, ProductType, RegistrationUnit, Producer, RegistrationCert, ProductCategory
+from marketing.product.models import Product, ProductType, RegistrationUnit, Producer, RegistrationCert, \
+    ProductCategory, CategoryDetail
 
 # Register your models here.
 admin.site.register(ProductType)
@@ -9,3 +10,4 @@ admin.site.register(Producer)
 admin.site.register(RegistrationCert)
 admin.site.register(ProductCategory)
 admin.site.register(Product)
+admin.site.register(CategoryDetail)
