8169d6dd62c42cf73ceba0e7bc269dca20b28b7a
jdsatashi <vdt1073@gmail.com>
2024-04-30 23:00:16 +0700

    Update move main function to utils.get_old_data

--- src/marketing/company/migrations/migrate_old_company.py
+++ src/marketing/company/migrations/migrate_old_company.py
@@ -1,21 +1,6 @@
 from django.db import migrations
 
-from marketing.company.models import Company
-from utils.constants import old_data
-from utils.helpers import table_data
-
-
-def add_old_company(apps, schema_editor):
-    data = table_data(old_data['tb_congty'])
-    for k, v in enumerate(data):
-        try:
-            Company.objects.create(id=v[0], name=v[1], note=v[2], color_code=v[5], created_at=v[3])
-            print(f"Added company {v[1]} - {v[0]}")
-        except Exception as e:
-            print(f"----- ERROR -----")
-            print(f"Message: Error when adding company.")
-            print(e)
-            raise Exception(e)
+from utils.get_old_data import add_old_company
 
 
 class Migration(migrations.Migration):
