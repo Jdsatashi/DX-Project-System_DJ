7bb3a9cdb1ec068162e75461ca15cc18f3229343
jdsatashi <vdt1073@gmail.com>
2024-04-30 23:08:21 +0700

    Update add login view

--- src/account/api/routes.py
+++ src/account/api/routes.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from account.api.views import api_create_user, ApiAccount, otp_verify, RegisterSMS
+from account.api.views import api_create_user, ApiAccount, otp_verify, RegisterSMS, phone_login
 
 app_name = 'api_account'
 
@@ -17,5 +17,6 @@ urlpatterns = [
     path('register', RegisterSMS.as_view(), name='api_register'),
     path('register/verify/<pk>', otp_verify, name='api_verify'),
     path('', account_view),
-    path('<int:pk>', account_detail)
+    path('<int:pk>', account_detail),
+    path('login', phone_login)
 ]
--- src/account/api/views.py
+++ src/account/api/views.py
@@ -1,5 +1,7 @@
+import os
 from functools import partial
 
+import requests
 from django.http import HttpResponse
 from django.utils import timezone
 from rest_framework import mixins, viewsets, status
@@ -18,6 +20,7 @@ from app.logs import acc_log
 from user_system.client_profile.models import ClientProfile
 from user_system.employee_profile.models import EmployeeProfile
 from utils.constants import status as user_status
+from utils.env import APP_SERVER
 from utils.model_filter_paginate import filter_data
 
 
@@ -80,20 +83,58 @@ def otp_verify(request, pk):
 
         if otp_code == verify.verify_code and not verify.is_verify:
             if verify.is_verify_valid():
-                verify.is_verify = True
-                verify.verify_time = timezone.now()
-                verify.save()
-                verify.user.status = user_status[0]
-                verify.user.is_active = True
-                verify.user.save()
                 print(f"Valid verify")
                 serializer = UserSerializer(verify.user)
                 response = {'message': 'Successful verify phone number', 'user': serializer.data}
                 token = get_token_for_user(verify.user)
+                verify.is_verify = True
+                verify.verify_time = timezone.now()
+                verify.refresh_token = token['refresh']
+                verify.save()
+                verify.user.status = user_status[0]
+                verify.user.is_active = True
+                verify.user.save()
                 response['phone_number'] = pk
                 response['token'] = token
                 return Response(response, status=status.HTTP_200_OK)
         print("OTP code is expired")
         return Response({'message': 'Mã otp đã hết hạn'}, status=status.HTTP_200_OK)
 
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST', 'GET'])
+def phone_login(request):
+    if request.method == 'GET':
+        response = {
+            'phone_number': '0123456789',
+            'refresh_token': 'token...'
+        }
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    if request.method == 'POST':
+        phone_number = request.data.get('phone_number', None)
+        refresh_token = request.data.get('refresh_token', None)
+        if phone_number is None:
+            return Response({'message': 'Bạn cần nhập số điện thoại'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            phone = PhoneNumber.objects.get(phone_number=phone_number)
+        except PhoneNumber.DoesNotExist:
+            # return Response({'message': 'Số điện thoại không tồn tại'}, status=status.HTTP_404_NOT_FOUND)
+
+            print(f"TEST LOGIN -------------")
+            print("Try register in login")
+            main_url = APP_SERVER
+            register_path = f"/application/api/v1/2024/accounts/register"
+            api_url = main_url + register_path
+            print(api_url)
+
+            data = {
+                'phone_number': phone_number
+            }
+            response = requests.post(api_url, data)
+            print(f"Response: =  {response}")
+            response_data = response.json()
+            return Response(response_data)
+        user = phone.user
+
+        verify = Verify.objects.filter(phone_verify=phone, refresh_token=refresh_token)
