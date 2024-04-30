d391c62a95416114710800a8c2461ba237b42007
jdsatashi <vdt1073@gmail.com>
2024-04-30 23:04:05 +0700

    Update priority of migrate

--- src/account/migrations/migrate_perm.py
+++ src/account/migrations/migrate_perm.py
@@ -1,12 +1,14 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import migrations
 
+from account.models import Perm, GroupPerm
 from app.settings import MY_APPS
 from utils.constants import acquy
 
 
 def create_initial_permission(apps, schema_editor):
-    ContentType = apps.get_model('contenttypes', 'ContentType')
-    Perm = apps.get_model('account', 'Perm')
+    # ContentType = apps.get_model('contenttypes', 'ContentType')
+    # Perm = apps.get_model('account', 'Perm')
     content_types = ContentType.objects.all()
 
     for i, content_type in enumerate(content_types):
@@ -20,6 +22,24 @@ def create_initial_permission(apps, schema_editor):
                 Perm.objects.create(name=perm_name_, note=f'{task.capitalize()} {content_type.model}')
 
 
+def create_draft_perm(apps, schema_editor):
+    matching_perm = Perm.objects.filter(name__icontains="draft").all()
+    list_drafts_perm = list(matching_perm)
+    group_perm, _ = GroupPerm.objects.get_or_create(name="group_draft")
+    for q in list_drafts_perm:
+        group_perm.perm.add(q.name)
+
+
+def create_admin_perm(apps, schema_editor):
+    admin_role = "admin"
+    matching_perms = Perm.objects.all()
+    list_perms = list(matching_perms)
+    group_perm, _ = GroupPerm.objects.get_or_create(name=admin_role)
+    for q in list_perms:
+        print(f"Add perms '{q.name}' to group {admin_role}")
+        group_perm.perm.add(q.name)
+
+
 class Migration(migrations.Migration):
     depdc = [('account', '0001_initial'), ('admin', '0001_initial')]
     for i, v in enumerate(MY_APPS):
@@ -31,4 +51,6 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(create_initial_permission),
+        migrations.RunPython(create_draft_perm),
+        migrations.RunPython(create_admin_perm),
     ]
