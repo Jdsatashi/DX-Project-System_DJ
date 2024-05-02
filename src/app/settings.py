076f12adf5fb6a05bece02ab112e8eddfe06300b
jdsatashi <vdt1073@gmail.com>
2024-05-03 02:39:03 +0700

    Update disable rotate refresh token

--- src/app/settings.py
+++ src/app/settings.py
@@ -130,7 +130,7 @@ LOGGING = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": True,
+    "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
 
