1dea59173de039c5b30d96d0046ce5c7f9245c7a
jdsatashi <vdt1073@gmail.com>
2024-05-08 09:52:58 +0700

    Update comments and edit queryset for suit with other none class generic view

--- src/utils/model_filter_paginate.py
+++ src/utils/model_filter_paginate.py
@@ -16,25 +16,30 @@ def get_query_parameters(request):
 
 
 def dynamic_q(query, fields, strict_mode):
-    dynamic_q = Q()
+    dynamic_query = Q()
     query_type = '__icontains' if not strict_mode else '__exact'
     for field in fields:
-        print(f'{field}{query_type}')
-        dynamic_q |= Q(**{f'{field}{query_type}': query})
-    return dynamic_q
+        dynamic_query |= Q(**{f'{field}{query_type}': query})
+    return dynamic_query
 
 
-def filter_data(self, request, query_fields, *args, **kwargs):
+def filter_data(self, request, query_fields, **kwargs):
+    # Get query set if exists or get default
+    queryset = kwargs.get('queryset', self.get_queryset())
+    # Split query search, strict mode, limit, page, order_by
     query, strict_mode, limit, page, order_by = get_query_parameters(request)
-    products = self.get_queryset()
+    # If query exists, filter queryset
     if query != '':
         query_filter = dynamic_q(query, query_fields, strict_mode)
-        products = products.filter(query_filter)
+        queryset = queryset.filter(query_filter)
+    # Get fields in models
     valid_fields = [f.name for f in self.serializer_class.Meta.model._meta.get_fields()]
+    # Check field order_by exists, then order queryset
     if order_by in valid_fields or order_by.lstrip('-') in valid_fields:
-        products = products.order_by(order_by)
+        queryset = queryset.order_by(order_by)
+    # Check when limit is 0, return all data
     if limit == 0:
-        serializer = self.serializer_class(products, many=True)
+        serializer = self.serializer_class(queryset, many=True)
         response_data = {
             'data': serializer.data,
             'total_page': 1,
@@ -43,22 +48,29 @@ def filter_data(self, request, query_fields, *args, **kwargs):
             'prev_page': None
         }
         return response_data
-    paginator = Paginator(products, limit)
+    # Paginate queryset
+    paginator = Paginator(queryset, limit)
+    # Get page object
     page_obj = paginator.get_page(page)
+    # Get serializer data in page objects data
     serializer = self.serializer_class(page_obj, many=True)
+    # Validate page number
     if page < 0:
         page = 1
     elif page > paginator.num_pages:
         page = paginator.num_pages
+    # Add total and current page and data to response data
     response_data = {
         'data': serializer.data,
         'total_page': paginator.num_pages,
         'current_page': page
     }
+    # If has next page, add urls to response data
     if page_obj.has_next():
         next_page = request.build_absolute_uri(
             '?page={}&limit={}&query={}'.format(page_obj.next_page_number(), limit, query))
         response_data['next_page'] = next_page
+    # If has previous page, add urls to response data
     if page_obj.has_previous():
         prev_page = request.build_absolute_uri(
             '?page={}&limit={}&query={}'.format(page_obj.previous_page_number(), limit, query))
