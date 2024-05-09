c469b35e16e3278d0c2ec1a888a2a7530baac23a
jdsatashi <vdt1073@gmail.com>
2024-05-09 09:31:41 +0700

    Update admin site

--- src/system/file_upload/admin.py
+++ src/system/file_upload/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from system.file_upload.models import FileUpload, ProductCateFile, ProductFile, RegistrationCertFile
+
 # Register your models here.
+admin.site.register(FileUpload)
+admin.site.register(ProductCateFile)
+admin.site.register(ProductFile)
+admin.site.register(RegistrationCertFile)
