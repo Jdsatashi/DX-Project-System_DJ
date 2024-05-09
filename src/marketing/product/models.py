c32fbc4c411dcc9297f86a7150e04be06f985523
jdsatashi <vdt1073@gmail.com>
2024-05-09 10:03:21 +0700

    Update string show models

--- src/marketing/product/models.py
+++ src/marketing/product/models.py
@@ -48,6 +48,9 @@ class ProductCategory(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.id} - {self.name}"
+
 
 class UseObject(models.Model):
     id = models.CharField(primary_key=True, max_length=64, unique=True)
