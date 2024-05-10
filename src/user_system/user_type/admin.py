390dba942c153fc7f65da3e354f26091ccd3428b
jdsatashi <vdt1073@gmail.com>
2024-05-10 15:12:43 +0700

    Update remove user_type

--- src/user_system/user_type/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from user_system.user_type.models import UserType
-
-# Register your models here.
-admin.site.register(UserType)
--- src/user_system/user_type/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from user_system.user_type.models import UserType
-
-
-class UserTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserType
-        fields = '__all__'
