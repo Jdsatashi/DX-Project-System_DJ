94147025ea32e6cadfa191c53cb0f2a66be87771
jdsatashi <vdt1073@gmail.com>
2024-05-10 09:39:41 +0700

    Update admin site searching file upload

--- src/system/file_upload/admin.py
+++ src/system/file_upload/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from system.file_upload.models import FileUpload, ProductCateFile, ProductFile, RegistrationCertFile
 
+
+class FileUploadAmin(admin.ModelAdmin):
+    search_fields = ['file', 'file_name']
+
+
 # Register your models here.
-admin.site.register(FileUpload)
+admin.site.register(FileUpload, FileUploadAmin)
 admin.site.register(ProductCateFile)
 admin.site.register(ProductFile)
 admin.site.register(RegistrationCertFile)
