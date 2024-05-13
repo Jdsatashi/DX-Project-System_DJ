26bbc56ab027a0c362661cd0536c54e9ec222265
jdsatashi <vdt1073@gmail.com>
2024-05-13 16:46:54 +0700

    Update settings urls for schema api

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.shortcuts import render
 from django.urls import path, include
-from rest_framework.permissions import AllowAny
-from rest_framework.schemas import get_schema_view
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
+
+schema_view = SpectacularAPIView.as_view()
 
 
 # Create homepage view
@@ -13,22 +14,16 @@ def home(request):
     return render(request, 'homepage.html')
 
 
-schema_view = get_schema_view(
-    title="API schema",
-    description="Guide for REST API",
-    version="1.0.0",
-    authentication_classes=[],
-    permission_classes=[AllowAny]
-)
-
 urlpatterns = [
     path('', home, name='home'),
     path('system/admin/', admin.site.urls),
     path('draft/', include('draft.urls', 'draft')),
     path('accounts/', include('account.urls', 'account')),
-    path('api_schema', schema_view, name='api_schema'),
+    path('api/schema/', schema_view, name='api-schema'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='api-schema'), name='redoc'),
     path('application/api/v1/2024/', include('app.api_routes.routes', 'api_routes')),
 ]
+
 # Add static files tto url
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
