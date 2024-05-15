f051ec31361947718cfb1d6e03f2caf7a6945eff
jdsatashi <vdt1073@gmail.com>
2024-05-15 19:34:24 +0700

    Update config gunicorn

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,4 +1,4 @@
-command = '/home/jdsatashi/development/DX-Project-System_DJ/venv/bin/gunicorn'
-pythonpath = '/home/jdsatashi/development/DX-Project-System_DJ'
+command = '/home/jdsatashi/dev/project/venv/bin/gunicorn'
+pythonpath = '/home/jdsatashi/dev/project'
 bind = '0.0.0.0:8000'
 worker = 3
