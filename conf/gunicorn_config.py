cc6f14a05a25a81949c1ef2e106223d331f6a619
jdsatashi <vdt1073@gmail.com>
2024-05-15 22:43:07 +0700

    Update config

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,4 +1,6 @@
-command = '/home/jdsatashi/dev/project/venv/bin/gunicorn'
-pythonpath = '/home/jdsatashi/dev/project'
+command = '/home/jdsatashi/dev/dx-backend/venv/bin/gunicorn'
+pythonpath = '/home/jdsatashi/dev/dx-backend/src'
 bind = '0.0.0.0:8000'
 worker = 3
+app = 'app.wsgi.application'
+http_protocol = "h2"
