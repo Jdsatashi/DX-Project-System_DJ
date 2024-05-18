9be98cf18a243c0e1568df18f2a8698745a2c3b3
jdsatashi <vdt1073@gmail.com>
2024-05-18 18:08:35 +0700

    Update functions processing

--- src/marketing/livestream/api/serializers.py
+++ src/marketing/livestream/api/serializers.py
@@ -73,11 +73,55 @@ class LiveProduct(BaseRestrictSerializer):
         read_only_fields = ['id', 'created_at', 'updated_at']
 
 
+class LiveProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LiveStreamProduct
+        fields = ['product', 'price']
+
+
 class LiveProductList(BaseRestrictSerializer):
+    products_list = LiveProductSerializer(many=True, write_only=True)
+    livestreamproduct_set = LiveProduct(many=True, read_only=True)
+
     class Meta:
         model = LiveStreamProductList
         fields = '__all__'
         read_only_fields = ['id', 'created_at', 'updated_at']
+
+    def create(self, validate_data):
+        data, perm_data = self.split_data(validate_data)
+        products_data = data.pop('products_list')
+        live_product_list = LiveStreamProductList.objects.create(**data)
+
+        for product_data in products_data:
+            LiveStreamProduct.objects.create(livestream_product_list=live_product_list, **product_data)
+
+        restrict = perm_data.get('restrict')
+        if restrict:
+            self.handle_restrict(perm_data, live_product_list.id, self.Meta.model)
+
+        return live_product_list
+
+    def update(self, instance, validate_data):
+        data, perm_data = self.split_data(validate_data)
+        products_data = data.pop('products_list', None)
+
+        for attr, value in data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if products_data is not None:
+            # Clear existing products
+            instance.livestreamproduct_set.all().delete()
+            # Add updated products
+            for product_data in products_data:
+                LiveStreamProduct.objects.create(livestream_product_list=instance, **product_data)
+
+        restrict = perm_data.get('restrict')
+        if restrict:
+            self.handle_restrict(perm_data, instance.id, self.Meta.model)
+
+        return instance
 
 
 class LiveStatistic(BaseRestrictSerializer):
@@ -91,4 +135,59 @@ class LiveTracking(BaseRestrictSerializer):
     class Meta:
         model = LiveStreamTracking
         fields = '__all__'
-        read_only_fields = ['id', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'phone', 'time_watch', 'created_at', 'updated_at']
+
+    def create(self, validate_data):
+        data, perm_data = self.split_data(validate_data)
+
+        request = self.context.get('request')
+        if request:
+            auth_header = request.headers.get('Authorization')
+            if auth_header and auth_header.startswith('Bearer '):
+                access_token = auth_header.split(' ')[1]
+            else:
+                raise ValidationError('Invalid token')
+        else:
+            raise ValidationError('Not found request')
+
+        # Get user and phone objects from access token
+        try:
+            token = AccessToken(access_token)
+            user = User.objects.get(id=token['user_id'])
+            phone = PhoneNumber.objects.get(phone_number=token['phone_number'])
+        except TokenError:
+            return Response({'message': 'Invalid token'}, status=401)
+
+        time_join = data.get('time_join', None)
+        live_stream = data.get('live_stream', None)
+        note = data.get('note', None)
+
+        # Create tracking
+        tracking = LiveStreamTracking.objects.create(
+            phone=phone,
+            time_join=time_join,
+            live_stream=live_stream,
+            note=note
+        )
+
+        # Create permission
+        restrict = perm_data.get('restrict')
+        if restrict:
+            self.handle_restrict(perm_data, tracking.id, self.Meta.model)
+
+        return tracking
+
+    def update(self, instance, validate_data):
+        data, perm_data = self.split_data(validate_data)
+        data.pop('time_join')
+        data.pop('live_stream')
+        data.pop('phone')
+        for attr, value in data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        restrict = perm_data.get('restrict')
+        if restrict:
+            self.handle_restrict(perm_data, instance.id, self.Meta.model)
+
+        return instance
