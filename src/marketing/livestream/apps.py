5cc2bea44dc781d9aba44d6e7405eaf7ece1f4c6
jdsatashi <vdt1073@gmail.com>
2024-05-20 21:43:47 +0700

    Update statistic

--- src/marketing/livestream/apps.py
+++ src/marketing/livestream/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class LivestreamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'marketing.livestream'
+
+    def ready(self):
+        import marketing.livestream.signals
--- src/marketing/livestream/signals.py
+++ src/marketing/livestream/signals.py
@@ -1,6 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import LiveStream, LiveStreamStatistic, LiveStreamPeekView
+
+from .models import LiveStream, LiveStreamStatistic, LiveStreamPeekView, LiveStreamTracking, LiveStreamOfferRegister, \
+    LiveStreamComment
 
 
 @receiver(post_save, sender=LiveStream)
@@ -8,3 +10,33 @@ def create_related_records(sender, instance, created, **kwargs):
     if created:
         LiveStreamStatistic.objects.create(live_stream=instance)
         LiveStreamPeekView.objects.create(live_stream=instance)
+
+
+@receiver(post_save, sender=LiveStreamTracking)
+@receiver(post_delete, sender=LiveStreamTracking)
+def update_viewers(sender, instance, **kwargs):
+    live_stream = instance.live_stream
+    unique_viewers = LiveStreamTracking.objects.filter(live_stream=live_stream).values('phone').distinct().count()
+    statistic, created = LiveStreamStatistic.objects.get_or_create(live_stream=live_stream)
+    statistic.viewers = unique_viewers
+    statistic.save()
+
+
+@receiver(post_save, sender=LiveStreamOfferRegister)
+@receiver(post_delete, sender=LiveStreamOfferRegister)
+def update_order_times(sender, instance, **kwargs):
+    live_stream = instance.live_stream
+    unique_orders = LiveStreamOfferRegister.objects.filter(live_stream=live_stream, register=True).values('phone').distinct().count()
+    statistic, created = LiveStreamStatistic.objects.get_or_create(live_stream=live_stream)
+    statistic.order_times = unique_orders
+    statistic.save()
+
+
+@receiver(post_save, sender=LiveStreamComment)
+@receiver(post_delete, sender=LiveStreamComment)
+def update_comments(sender, instance, **kwargs):
+    live_stream = instance.live_stream
+    comment_count = LiveStreamComment.objects.filter(live_stream=live_stream).count()
+    statistic, created = LiveStreamStatistic.objects.get_or_create(live_stream=live_stream)
+    statistic.comments = comment_count
+    statistic.save()
