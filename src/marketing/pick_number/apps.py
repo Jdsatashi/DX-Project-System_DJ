80f1d95b121b17fbc308aa813a9e53a2a5a8f74c
jdsatashi <vdt1073@gmail.com>
2024-05-30 17:23:37 +0700

    Add signals

--- src/marketing/pick_number/apps.py
+++ src/marketing/pick_number/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PickNumberConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'marketing.pick_number'
+
+    def ready(self):
+        import marketing.pick_number.signals
