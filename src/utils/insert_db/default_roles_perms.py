a0b5afaf527b6298b8d175c6e2f05780ed0d7469
jdsatashi <vdt1073@gmail.com>
2024-06-03 20:05:11 +0700

    Create new add default role

--- src/utils/insert_db/default_roles_perms.py
+++ src/utils/insert_db/default_roles_perms.py
@@ -2,7 +2,9 @@ from django.contrib.contenttypes.models import ContentType
 from pyodbc import IntegrityError
 
 from account.models import User, Perm, GroupPerm
-from marketing.order.models import Order
+from app.logs import app_log
+from marketing.order.models import Order, OrderDetail
+from marketing.price_list.models import SpecialOffer, SpecialOfferProduct, PriceList, ProductPrice
 from marketing.product.models import Product, ProductCategory, ProductType, RegistrationUnit, Producer, \
     RegistrationCert, UseObject, UseFor, CategoryDetail
 from utils.constants import acquy
@@ -12,8 +14,8 @@ def create_initial_permission():
     content_types = ContentType.objects.all()
 
     for i, content_type in enumerate(content_types):
-        print("\n_________ || --- || __________")
-        print(f"Perm: {i} - {content_type.model}")
+        app_log.info("\n_________ || --- || __________")
+        app_log.info(f"Perm: {i} - {content_type.model}")
         if i > 6:
             perm_name = f'{content_type.app_label}_{content_type.model}'
             tasks = acquy['full']
@@ -25,28 +27,28 @@ def create_initial_permission():
                         defaults={'note': f'{task.capitalize()} {content_type.model}', 'content_type': content_type}
                     )
                     if created:
-                        print(f"Created new permission: {perm_name}")
+                        app_log.info(f"Created new permission: {perm_name}")
                     else:
-                        print(f"Permission {perm_name} already exists.")
+                        app_log.info(f"Permission {perm_name} already exists.")
                 except IntegrityError:
-                    print(f"Failed to create permission due to an integrity error: {perm_name}")
+                    app_log.info(f"Failed to create permission due to an integrity error: {perm_name}")
                     continue
 
 
 def set_user_perm(user_instance, add):
-    print(user_instance)
+    app_log.info(user_instance)
     user_ins = User.objects.get(username=user_instance)
     content_type = ContentType.objects.get_for_model(user_ins)
     perm_name = f'{content_type.app_label}_{content_type.model}_{user_instance.id}'
     tasks = acquy['full']
     for task in tasks:
         perm_name_ = f'{task}_{perm_name}'
-        print(f"Adding permission: {perm_name}")
+        app_log.info(f"Adding permission: {perm_name}")
         try:
             perm_, _ = Perm.objects.get_or_create(
                 name=perm_name_,
                 defaults={'note': f'{task.capitalize()} {content_type.model}', 'content_type': content_type})
-            if add:
+            if add and task != 'destroy':
                 user_ins.perm_user.add(perm_, through_defaults={'allow': True})
         except IntegrityError:
             continue
@@ -54,15 +56,78 @@ def set_user_perm(user_instance, add):
 
 def set_farmer_role():
     farmer_group, _ = GroupPerm.objects.get_or_create(name='farmer', defaults={'allow': True})
-    order_content = ContentType.objects.get_for_model(Order)
-    view_group = [ProductType, RegistrationUnit, Producer, RegistrationCert, UseObject, UseFor, CategoryDetail]
-    for model in view_group:
+    view_group = [ProductType, RegistrationUnit, Producer, RegistrationCert, UseObject, UseFor, CategoryDetail,
+                  ProductCategory]
+    add_group_perm(farmer_group, acquy['view'], view_group)
+
+
+# Role Khách hàng/ Đại lý
+def set_client_role():
+    client_group, _ = GroupPerm.objects.get_or_create(name='client', defaults={'allow': True})
+    view_group = [ProductType, RegistrationUnit, Producer, RegistrationCert, UseObject, UseFor, CategoryDetail,
+                  ProductCategory]
+    add_group_perm(client_group, acquy['view'], view_group)
+
+    create_group = [Order, OrderDetail]
+    add_group_perm(client_group, [acquy['create']], create_group)
+
+
+def set_NVTT_role():
+    nvt_group, _ = GroupPerm.objects.get_or_create(name='nvtt', defaults={'allow': True})
+    view_group = [ProductType, RegistrationUnit, Producer, RegistrationCert, UseObject, UseFor, CategoryDetail,
+                  ProductCategory]
+    add_group_perm(nvt_group, acquy['view'], view_group)
+
+    view_special_group = [Product, PriceList, ProductPrice, SpecialOffer, SpecialOfferProduct]
+    add_group_perm(nvt_group, acquy['view'], view_special_group)
+
+
+def set_NPP_role():
+    npp_group, _ = GroupPerm.objects.get_or_create(name='npp', defaults={'allow': True})
+    view_group = [ProductType, RegistrationUnit, Producer, RegistrationCert, UseObject, UseFor, CategoryDetail,
+                  ProductCategory]
+    add_group_perm(npp_group, acquy['view'], view_group)
+
+    view_special_group = [Product, PriceList, ProductPrice, SpecialOffer, SpecialOfferProduct]
+    add_group_perm(npp_group, acquy['view'], view_special_group)
+
+
+def set_client_default():
+    farmer_group, _ = GroupPerm.objects.get_or_create(name='farmer',
+                                                      defaults={'allow': True, 'description': 'Nông dân'})
+    client_group, _ = GroupPerm.objects.get_or_create(name='client',
+                                                      defaults={'allow': True, 'description': 'Khách hàng'})
+
+    client_lv1, _ = GroupPerm.objects.get_or_create(name='client_lv1',
+                                                    defaults={'allow': True, 'description': 'Khách hàng cấp 1',
+                                                              'parent_group': client_group})
+    client_lv2, _ = GroupPerm.objects.get_or_create(name='client_lv2',
+                                                    defaults={'allow': True, 'description': 'Khách hàng cấp 2',
+                                                              'parent_group': client_group})
+    client_vip, _ = GroupPerm.objects.get_or_create(name='client_vip',
+                                                    defaults={'allow': True, 'description': 'Khách hàng VIP',
+                                                              'parent_group': client_group})
+    client_lv1_north, _ = GroupPerm.objects.get_or_create(name='client_lv1_north', defaults={'allow': True,
+                                                                                             'description': 'Khách hàng cấp 1 Miền Bắc',
+                                                                                             'parent_group': client_lv1})
+    test_group, _ = GroupPerm.objects.get_or_create(name='test', defaults={'allow': True, 'description': 'Nhóm test'})
+
+
+def auto_role():
+    set_client_default()
+    set_farmer_role()
+    set_client_role()
+    set_NVTT_role()
+    set_NPP_role()
+
+
+def add_group_perm(group, actions: list, models):
+    for model in models:
         model_content = ContentType.objects.get_for_model(model)
-        action_view = acquy['view']
-        for action in action_view:
+        for action in actions:
+            app_log.info(f"Test models: {model_content}")
+            app_log.info(f"Test {action}")
             perm_name = f'{action}_{model_content.app_label}_{model_content.model}'
+            app_log.info(f"Permission: {perm_name}")
             perm = Perm.objects.get(name=perm_name)
-            farmer_group.permissions.add(perm)
-    perm_name = f'create_{order_content.app_label}_{order_content.model}'
-    order_perm = Perm.objects.get(name=perm_name)
-    farmer_group.permissions.add(order_perm)
+            group.perm.add(perm, through_defaults={'allow': True})
