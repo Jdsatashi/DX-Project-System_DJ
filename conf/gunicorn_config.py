20200c0c03ca5cad1fd1470b93f1c4b964b28570
Jdsatashi <vdt1073@gmail.com>
2024-06-17 02:39:28 +0000

    Remove conf/ and runserver.sh from repository

--- conf/gunicorn_config.py
+++
@@ -1,6 +0,0 @@
-command = '/home/jdsatashi/dx-backend/venv/bin/gunicorn'
-pythonpath = '/home/jdsatashi/dx-backend/src'
-bind = '0.0.0.0:8000'
-worker = 3
-app = 'app.wsgi.application'
-http_protocol = "h2"
--- runserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gunicorn -c conf/gunicorn_config.py --reload src.app.wsgi --timeout 120
-
