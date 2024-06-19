f773d9d46a7984482317043946e8f3b1988d8849
jdsatashi <vdt1073@gmail.com>
2024-06-19 16:02:03 +0700

    Update fix error auto add perm for admin

--- src/account/signals.py
+++ src/account/signals.py
@@ -2,10 +2,11 @@ from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from account.models import Perm, User, UserGroupPerm, GroupPerm
+from utils.constants import admin_role
 
 
 @receiver(post_save, sender=Perm)
-def add_quyen_to_superuser(sender, instance, created, admin_role=None, **kwargs):
+def add_quyen_to_superuser(sender, instance, created, **kwargs):
     if created:
         superusers = User.objects.filter(is_superuser=True)
 
