347ae6af4804502f26a9a2982dc0c6c581ef338a
jdsatashi <vdt1073@gmail.com>
2024-06-21 09:02:35 +0700

    Update group for user

--- src/utils/insert_db/client_npp.py
+++ src/utils/insert_db/client_npp.py
@@ -1,5 +1,7 @@
 from django.db import transaction
+from django.db.models import Q
 
+from account.models import User, GroupPerm
 from user_system.client_profile.models import ClientProfile
 
 users_list = [
@@ -18,3 +20,19 @@ def update_npp(user_list: list):
 
     with transaction.atomic():
         ClientProfile.objects.bulk_update(client_profile, ['is_npp'])
+
+    u = User.objects.filter(id__in=user_list)
+    npp = GroupPerm.objects.get(name='npp')
+    for user in u:
+        user.group_user.add(npp, through_defaults={'allow': True})
+
+
+def update_nvtt():
+    queryset = User.objects.filter(
+        Q(employeeprofile__position__id='NVTT')
+    ).select_related('employeeprofile').prefetch_related('employeeprofile__position').exclude(
+        group_user__name='admin').distinct()
+    nvtt = GroupPerm.objects.get(name='nvtt')
+
+    for user in queryset:
+        user.group_user.add(nvtt, through_defaults={'allow': True})
