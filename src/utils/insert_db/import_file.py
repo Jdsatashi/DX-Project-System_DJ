04def8da81e29bcc84cadbdda85d831910fc475a
jdsatashi <vdt1073@gmail.com>
2024-06-26 09:02:15 +0700

    Update

--- src/utils/insert_db/import_file.py
+++ src/utils/insert_db/import_file.py
@@ -18,36 +18,35 @@ def import_files_from_directory(directory_path, file_type):
     not_found = []
     for root, dirs, files in os.walk(directory_path):
         for i, file in enumerate(files):
-            if i < 5:
-                file_path = os.path.join(root, file)
-                with open(file_path, 'rb') as f:
-                    django_file = File(f)
-                    filename, ext = os.path.splitext(file)
+            file_path = os.path.join(root, file)
+            with open(file_path, 'rb') as f:
+                django_file = File(f)
+                filename, ext = os.path.splitext(file)
 
-                    app_log.info(f"{filename} | {ext}")
-                    if file_type == 'register':
-                        filename_list = filename.split(' ')
-                        search_filename = filename_list[1:]
-                    else:  # == 'image'
-                        search_filename = filename.split('_')
-                    while len(search_filename) >= 1:
-                        search_filename = " ".join(search_filename)
-                        result = ProductCategory.objects.filter(name__icontains=search_filename)
-                        if result.exists():
-                            product_cate = result.first()
-                            app_log.info(f"Found product cate: {product_cate}")
+                app_log.info(f"{filename} | {ext}")
+                if file_type == 'register':
+                    filename_list = filename.split(' ')
+                    search_filename = filename_list[1:]
+                else:  # == 'image'
+                    search_filename = filename.split('_')
+                while len(search_filename) >= 1:
+                    search_filename = " ".join(search_filename)
+                    result = ProductCategory.objects.filter(name__icontains=search_filename)
+                    if result.exists():
+                        product_cate = result.first()
+                        app_log.info(f"Found product cate: {product_cate}")
 
-                            file_instance = FileUpload.objects.create(file=django_file)
+                        file_instance = FileUpload.objects.create(file=django_file)
 
-                            ProductCateFile.objects.create(file=file_instance, product_cate=product_cate)
-                            break
-                        else:
-                            app_log.info(f"Product cate not found")
-                            if len(search_filename) == 1:
-                                not_found.append(f"File: {filename}")
-                            search_filename = search_filename.split(' ')
-                            search_filename.pop()
-                            continue
+                        ProductCateFile.objects.create(file=file_instance, product_cate=product_cate)
+                        break
+                    else:
+                        app_log.info(f"Product cate not found")
+                        if len(search_filename) == 1:
+                            not_found.append(f"File: {filename}")
+                        search_filename = search_filename.split(' ')
+                        search_filename.pop()
+                        continue
 
     app_log.info(f"Not found list: {not_found}")
 
