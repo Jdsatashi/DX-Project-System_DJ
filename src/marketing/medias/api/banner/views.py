464f03ae2f07a1ae107817d37e727674cd54dd7d
jdsatashi <vdt1073@gmail.com>
2024-06-30 15:42:39 +0700

    Update query filter banner

--- src/marketing/medias/api/banner/views.py
+++ src/marketing/medias/api/banner/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.response import Response
@@ -24,7 +25,28 @@ class ApiBanner(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMod
         return super().get_authenticators()
 
     def list(self, request, *args, **kwargs):
-        response = filter_data(self, request, ['id', 'name'], **kwargs)
+        queryset = self.get_queryset()
+
+        banner_type = request.query_params.get('type', None)
+        display_type = request.query_params.get('display_type', None)
+        # Get strict as boolean
+        strict = request.data.get('strict') or request.query_params.get('strict', 0)
+        strict = int(strict)
+        strict = strict if strict in [0, 1] else 0
+        strict_mode = bool(strict)
+
+        query_filter = Q()
+        query_type = '__exact' if strict_mode else '__icontains'
+
+        if banner_type:
+            query_filter &= Q(**{f'type{query_type}': banner_type})
+
+        if display_type:
+            query_filter &= Q(**{f'display_type{query_type}': display_type})
+
+        queryset = queryset.filter(query_filter)
+
+        response = filter_data(self, request, ['id', 'name'], queryset=queryset, **kwargs)
         return Response(response, status.HTTP_200_OK)
 
     # def get_permissions(self):
