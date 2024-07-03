dcf546640ca87ee6f0387dfc02bc86eacc780862
jdsatashi <vdt1073@gmail.com>
2024-07-03 21:30:47 +0700

    Update remove

--- client_group/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from client_group.models import ClientGroup
-
-
-# Register your models here.
-admin.site.register(ClientGroup)
--- client_group/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from account.handlers.restrict_serializer import BaseRestrictSerializer
-from client_group.models import ClientGroup
-
-
-class ClientGroupSerializer(BaseRestrictSerializer):
-    class Meta:
-        model = ClientGroup
-        fields = '__all__'
