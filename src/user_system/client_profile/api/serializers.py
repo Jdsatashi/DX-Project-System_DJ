280dcfdf0d5c1b1420c4e94af44dd9767ed98d1c
jdsatashi <vdt1073@gmail.com>
2024-07-19 19:06:42 +0700

    Update display name for client groups and nvtt

--- src/user_system/client_profile/api/serializers.py
+++ src/user_system/client_profile/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from account.handlers.restrict_serializer import BaseRestrictSerializer
+from account.models import User
 from user_system.client_profile.models import ClientProfile, ClientGroup
 
 
@@ -12,12 +13,24 @@ class ClientProfileSerializer(BaseRestrictSerializer):
 
 
 class ClientProfileUserSerializer(serializers.ModelSerializer):
+    client_group_id = serializers.CharField(source='client_group_id.name')
+    nvtt_id = serializers.SerializerMethodField()
+
     class Meta:
         model = ClientProfile
         fields = ['register_name', 'address', 'client_group_id', 'is_npp', 'nvtt_id']
         extra_kwargs = {
             'is_npp': {'required': False},
         }
+
+    def get_nvtt_id(self, obj):
+        nvtt_id = obj.nvtt_id
+        nvtt = User.objects.filter(id=nvtt_id).first()
+        response = {
+            'nvtt_id': nvtt_id,
+            'nvtt_name': nvtt.employeeprofile.register_name
+        }
+        return response
 
 
 class ClientGroupSerializer(BaseRestrictSerializer):
