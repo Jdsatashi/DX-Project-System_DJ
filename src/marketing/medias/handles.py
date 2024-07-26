f77c3f6f0fbc997fc3e12d10c8ad2796254eeaa3
jdsatashi <vdt1073@gmail.com>
2024-07-26 13:01:41 +0700

    Update testing 12

--- src/marketing/medias/handles.py
+++ src/marketing/medias/handles.py
@@ -3,7 +3,7 @@ from firebase_admin import messaging
 from app.logs import app_log
 
 
-def send_firebase_notification(title, body, registration_tokens, data):
+def send_firebase_notification2(title, body, registration_tokens, data):
     """
     Function to send a Firebase notification with custom data.
 
@@ -26,3 +26,33 @@ def send_firebase_notification(title, body, registration_tokens, data):
     # app_log.info(f"FIREBASE response: {response}")
     # app_log.info('{0} messages were sent successfully'.format(response.success_count))
     return response
+
+
+def send_firebase_notification(title, body, registration_tokens, data):
+    """
+    Function to send a Firebase notification with custom data.
+
+    :param title: Title of the notification
+    :param body: Body of the notification
+    :param registration_tokens: List of device registration tokens
+    :param data: Additional custom data to send with the notification
+    """
+    app_log.info(f"Handling upload notify to FIREBASE")
+
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+        ),
+        data=data,
+        tokens=registration_tokens
+    )
+    try:
+        # response = messaging.send_multicast(message)
+        response = messaging.send_each_for_multicast(message)
+        app_log.info(f"FIREBASE response: {response}")
+        app_log.info('{0} messages were sent successfully'.format(response.success_count))
+        return response
+    except Exception as e:
+        app_log.error(f"Error sending notification to FIREBASE: {e}")
+        return None
