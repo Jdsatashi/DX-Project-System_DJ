a035142502717338e01e89f7d4f75ad15fa6aee5
jdsatashi <vdt1073@gmail.com>
2024-08-06 10:41:48 +0700

    Update handle token error

--- src/account/handlers/token.py
+++ src/account/handlers/token.py
@@ -1,5 +1,9 @@
+from rest_framework_simplejwt.exceptions import TokenError
+
 from account.models import RefreshToken
 from rest_framework_simplejwt.tokens import RefreshToken as RestRefreshToken, AccessToken
+
+from app.logs import app_log
 
 
 def deactivate_user_token(user):
@@ -16,7 +20,11 @@ def deactivate_user_phone_token(user, phone):
     token_obj = RefreshToken.objects.filter(user=user, phone_number=phone, status="active").first()
     # Update deactivate other activate token
     if token_obj:
-        token_obj.status = "expired"
-        token_obj.save()
-        _token = RestRefreshToken(token_obj.refresh_token)
-        _token.blacklist()
+        try:
+            token_obj.status = "expired"
+            token_obj.save()
+            _token = RestRefreshToken(token_obj.refresh_token)
+            _token.blacklist()
+        except TokenError:
+            app_log.warning(f"deactive user phone token error: {phone}")
+            pass
