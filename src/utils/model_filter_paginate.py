f17b87e770b7035c95f7cdfc99efd01b41f0a16b
jdsatashi <vdt1073@gmail.com>
2024-08-12 21:10:05 +0700

    Update filter

--- src/utils/model_filter_paginate.py
+++ src/utils/model_filter_paginate.py
@@ -104,7 +104,7 @@ def get_query_parameters(request):
     strict_mode = bool(strict)
     from_date = data.get('from_date') or request.query_params.get('from_date', '')
     to_date = data.get('to_date') or request.query_params.get('to_date', '')
-    date_field = request.GET.get('date_field', 'created_at')
+    date_field = data.get('date_field', 'created_at') or request.query_params.get('to_date', '')
     return query, strict_mode, limit, page, order_by, from_date, to_date, date_field
 
 
