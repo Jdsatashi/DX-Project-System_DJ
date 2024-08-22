66d76aed6a95c8d33e13e27bb3bf9264d048bad9
jdsatashi <vdt1073@gmail.com>
2024-08-22 12:10:34 +0700

    Update

--- src/marketing/medias/api/notify/serializers.py
+++ src/marketing/medias/api/notify/serializers.py
@@ -139,9 +139,9 @@ class NotificationSerializer(serializers.ModelSerializer):
 
                 if delay < 10:
                     # Gửi thông báo ngay lập tức
-                    send_scheduled_notification.apply_async((notify.id, registration_tokens), countdown=10)
+                    send_scheduled_notification.apply_async((notify, registration_tokens), countdown=10)
                 else:
-                    send_scheduled_notification.apply_async((notify.id, registration_tokens), countdown=delay)
+                    send_scheduled_notification.apply_async((notify, registration_tokens), countdown=delay)
 
                 # send_firebase_notification3(notify.title, notify.short_description, registration_tokens, my_data)
                 # schedule_notification(notify)
