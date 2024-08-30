ccd851834c7b53213bbf9b9b272400ebec27256c
jdsatashi <vdt1073@gmail.com>
2024-08-30 10:46:24 +0700

    Update fix export statistic

--- src/marketing/sale_statistic/api/views.py
+++ src/marketing/sale_statistic/api/views.py
@@ -3,6 +3,7 @@ from functools import partial
 import openpyxl
 import pandas as pd
 from django.db import transaction
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.utils import timezone
 from openpyxl.utils import get_column_letter
@@ -14,9 +15,11 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from account.handlers.validate_perm import ValidatePermRest
 from account.models import User
 from app.logs import app_log
+from marketing.order.models import OrderDetail
 from marketing.sale_statistic.api.serializers import SaleStatisticSerializer, SaleMonthTargetSerializer, \
     UserSaleStatisticSerializer, UserUsedStatsSerializer
 from marketing.sale_statistic.models import SaleStatistic, SaleTarget, UserSaleStatistic, UsedTurnover
+from system_func.models import PeriodSeason
 from utils.helpers import local_time
 from utils.model_filter_paginate import filter_data
 
@@ -141,23 +144,38 @@ class ApiMainSaleStatistic(viewsets.GenericViewSet, mixins.ListModelMixin, mixin
         sheet.title = "User Sale Statistics"
 
         # Ghi tiêu đề cột
-        columns = ['Username', 'Turnover']
+        columns = ['MaKH', 'Doanh Số', 'Thùng ưu đãi đã dùng']
         for col_num, column_title in enumerate(columns, 1):
             column_letter = get_column_letter(col_num)
             sheet[f'{column_letter}1'] = column_title
 
-        # Ghi dữ liệu vào Excel
-        for row_num, row_data in enumerate(data, 2):
-            sheet[f'A{row_num}'] = row_data['user__id']  # Cột Username
-            sheet[f'B{row_num}'] = row_data['turnover']  # Cột Turnover
+        current_season: PeriodSeason = PeriodSeason.get_period_by_date('turnover')
 
-        # Tạo response HTTP và thiết lập tiêu đề
+        for row_num, instance in enumerate(data, 2):
+            user_id = instance['user__id']
+            turnover = instance['turnover']
+
+            # Lấy đối tượng User
+            user = User.objects.get(id=user_id)
+
+            # Lấy dữ liệu used_box cho user
+            user_so = user.order_set.filter(
+                is_so=True,
+                date_get__gte=current_season.from_date,
+                date_get__lte=current_season.to_date
+            )
+
+            used_box = OrderDetail.objects.filter(order_id__in=user_so).aggregate(total_box=Sum('order_box'))
+
+            sheet[f'A{row_num}'] = user.id  # Cột MaKH
+            sheet[f'B{row_num}'] = turnover  # Cột Doanh Số
+            sheet[f'C{row_num}'] = used_box['total_box'] or 0  # Cột Used Box
+
         response = HttpResponse(
             content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         )
         response['Content-Disposition'] = 'attachment; filename=user_sale_statistics.xlsx'
 
-        # Lưu file Excel vào response
         workbook.save(response)
 
         return response
