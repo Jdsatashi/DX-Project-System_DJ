00e6e9f3d0a526080d0400c5837fa9dbdb085ba0
jdsatashi <vdt1073@gmail.com>
2024-09-25 12:43:20 +0700

    Update test before update

--- src/utils/truncate/users.py
+++ src/utils/truncate/users.py
@@ -1,6 +1,8 @@
 from django.db import transaction
 
-from account.models import GrantAccess
+from account.models import GrantAccess, User
+from app.settings import PROJECT_DIR
+from utils.import_excel import file_data_to_dict
 
 
 def inactive_users():
@@ -10,3 +12,41 @@ def inactive_users():
             print(f"Handle: {grant_access.grant_user}")
             grant_access.active = False
             grant_access.save()
+
+
+def force_update_user():
+    file = PROJECT_DIR / 'test' / 'update_users.xlsx'
+    mapping = {
+        'maKH': 'user_id',
+        'tenKH': 'register_name',
+        'npp': 'npp',
+        'nvtt': 'nvtt',
+        'sdt1': 'phone_1',
+        'sdt2': 'phone_2',
+        'sdt3': 'phone_3',
+        'sdtChinh': 'main_phone',
+    }
+    datas = file_data_to_dict(file, mapping)
+    success = list()
+    errors = list()
+    nvtt_list = {data['nvtt'] for data in datas}
+    npp_list = {data['npp'] for data in datas}
+    lista = list()
+    for nvtt_name in nvtt_list:
+        nvtt_user = User.objects.filter(employeeprofile__register_name__icontains=nvtt_name)
+        if nvtt_user.exists():
+            lista.append(nvtt_user)
+    npp_user = User.objects.filter(clientprofile__register_name__in=npp_list, clientprofile__is_npp=True)
+    for user_data in datas:
+        try:
+            pass
+        except Exception as e:
+            raise e
+    print(f"Test nvtt: {len(nvtt_list)} - {len(lista)}")
+    print(f"Test npp: {len(npp_list)} - {len(npp_user)}")
+    a = npp_list.copy()
+    for npp in npp_user:
+        print(f"{npp.id} - {npp.clientprofile.register_name}")
+        a.remove(npp.clientprofile.register_name)
+    print(a)
+# from utils.truncate.users import force_update_user
