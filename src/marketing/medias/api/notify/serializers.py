eaeadee0eae8856c527046e8fb33435f6aea883e
jdsatashi <vdt1073@gmail.com>
2024-09-27 09:27:39 +0700

    Update active notification

--- src/marketing/medias/api/notify/serializers.py
+++ src/marketing/medias/api/notify/serializers.py
@@ -153,12 +153,11 @@ class NotificationSerializer(serializers.ModelSerializer):
                     'short_description': notify.short_description,
                 }
 
-                #
-                # if delay < 10:
-                #     # Gửi thông báo ngay lập tức
-                #     send_scheduled_notification.apply_async((notify.id, send_notify, registration_tokens), countdown=10)
-                # else:
-                #     send_scheduled_notification.apply_async((notify.id, send_notify, registration_tokens), countdown=delay)
+                if delay < 10:
+                    # Gửi thông báo ngay lập tức
+                    send_scheduled_notification.apply_async((notify.id, send_notify, registration_tokens), countdown=10)
+                else:
+                    send_scheduled_notification.apply_async((notify.id, send_notify, registration_tokens), countdown=delay)
 
                 # send_firebase_notification3(notify.title, notify.short_description, registration_tokens, my_data)
                 # schedule_notification(notify)
